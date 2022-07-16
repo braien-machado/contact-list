c2af833d566c234504470faf3f73a7780b23bafb
Braien Machado de Paula <braienmp@outlook.com>
2022-07-15 22:09:50 -0300

    feat: style header

--- app/frontend/src/components/Header.tsx
+++ app/frontend/src/components/Header.tsx
@@ -3,7 +3,47 @@ import styled from 'styled-components';
 import { createContact } from '../helpers/api';
 
 const MainHeader = styled.header`
+  border-bottom: 1px solid #e6e6e6;
+  color: #844ee7;
   display: flex;
+  justify-content: space-between;
+  padding: 20px 100px;
+
+  div {
+    display: flex;
+    gap: 20px;
+  }
+
+  input {
+    border: 1px solid #e6e6e6;
+    padding: 5px;
+    border-radius: 5px;
+  }
+
+  input:focus {
+    outline-color: #844ee7;
+  }
+
+  button {
+    background-color: #844ee7;
+    border-radius: 5px;
+    border: 1px solid #844ee7;
+    color: white;
+    font-weight: bold;
+    padding: 0 10px;
+  }
+
+  button:hover {
+    background-color: #7443ce;
+    border-color: #7443ce;
+    cursor: pointer;
+    transition-duration: 300ms;
+  }
+
+  button:disabled {
+    background-color: #acacac;
+    border-color: #acacac;
+  }
 `;
 
 interface HeaderProps {
@@ -27,13 +67,16 @@ export default function Header(props: HeaderProps) {
   const handleSubmit = async () => {
     await createContact(name);
     updateList();
+    setName('');
   };
 
   return (
     <MainHeader>
       <h1>Contacts</h1>
-      <input type="text" placeholder="Name" value={name} onChange={({ target }) => handleChange(target.value)} />
-      <button type="button" disabled={isDisabled} onClick={handleSubmit}>Add Contact</button>
+      <div>
+        <input type="text" placeholder="Name" value={name} onChange={({ target }) => handleChange(target.value)} />
+        <button type="button" disabled={isDisabled} onClick={handleSubmit}>Add Contact</button>
+      </div>
     </MainHeader>
   );
 }
